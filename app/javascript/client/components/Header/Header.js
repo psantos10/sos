1deb31fa92118c4df227d9f7d5d87a907aece567
Patrício dos Santos <hello@psantos.dev>
2020-03-29 23:31:08 +0100

    Use boxed layout

--- app/javascript/client/components/Header/Header.js
+++ app/javascript/client/components/Header/Header.js
@@ -7,38 +7,40 @@ const Header = () => (
     role="navigation"
     aria-label="main navigation"
   >
-    <div className="navbar-brand">
-      <a className="navbar-item" href="/">
-        SOS Angola
-      </a>
+    <div className="container">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          SOS Angola
+        </a>
 
-      <a
-        role="button"
-        className="navbar-burger burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarMain"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+        <a
+          role="button"
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarMain"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-    <div id="navbarMain" className="navbar-menu">
-      <div className="navbar-start">
-        <Link to="/" className="navbar-item">
-          Página Inicial
-        </Link>
-        <Link to="/preciso-de-ajuda" className="navbar-item">
-          Preciso de ajuda
-        </Link>
-        <Link to="/quero-ajudar" className="navbar-item">
-          Quero ajudar
-        </Link>
-        <Link to="/como-funciona" className="navbar-item">
-          Como funciona?
-        </Link>
+      <div id="navbarMain" className="navbar-menu">
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item">
+            Página Inicial
+          </Link>
+          <Link to="/preciso-de-ajuda" className="navbar-item">
+            Preciso de ajuda
+          </Link>
+          <Link to="/quero-ajudar" className="navbar-item">
+            Quero ajudar
+          </Link>
+          <Link to="/como-funciona" className="navbar-item">
+            Como funciona?
+          </Link>
+        </div>
       </div>
     </div>
   </nav>
