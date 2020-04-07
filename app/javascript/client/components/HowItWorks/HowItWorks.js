88987acc734a35e9c3f9776fb478094ed577e82f
José Cage <jose.cage@doesangue.me>
2020-04-07 18:22:15 +0100

    Added some text to How it Works page

--- app/javascript/client/components/HowItWorks/HowItWorks.js
+++ app/javascript/client/components/HowItWorks/HowItWorks.js
@@ -7,7 +7,12 @@ const HowItWorks = () => {
     <br />
       <h2 className="title">Como Funciona?</h2>
 
-      <p>Aqui vai o texto explicando como a plataforma funciona. <br/> Trabalho em progresso.</p>
+      <p>1. <b>SOS Angola</b> é um projeto de entreajuda comunitária que visa possibilitar a colaboração na superação de dificuldades causadas pelo Coronavirus (<b>Covid-19</b>).</p>
+      <p>Com principal atenção aos grupos de riscos. </p> <br />
+      <p>2. Podes pedir ajuda para a compra de alimentos ou medicação, apoio psicológico, transporte para deslocações necessárias, alojamento para os que estão longe de casa, entre outros.</p> <br />
+      <p>3. Quem vai ajudar? Quem quiser e/ou puder.</p> <br />
+      <p>4. Ao fazer um pedido, este será transmitido à rede de voluntários, garantindo a confidencialidade de dados pessoais.</p> <br />
+      <p>5. Após a análise do teu pedido, estabeleceremos a ligação entre ti e o a nossa rede de voluntários.</p> <br />
     </div>
   );
 }
