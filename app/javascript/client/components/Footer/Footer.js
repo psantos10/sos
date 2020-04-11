c1ccdb604209502d3a1c30d4d6c46bff8b0ca03c
Patrício dos Santos <hello@psantos.dev>
2020-04-11 18:03:07 +0100

    Add acknowledgment information on Footer

--- app/javascript/client/components/Footer/Footer.js
+++ app/javascript/client/components/Footer/Footer.js
@@ -4,7 +4,23 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        <p>SOS Angola v1.0.0.alpha.1</p>
+        <p>
+          <strong>SOS Angola</strong> v1.0.0.alpha.2
+          <br />
+          Projecto baseado na versão Portuguesa:{' '}
+          <a
+            href=" https://app.queroajudar.org/"
+            title="Quero Ajudar"
+            target="_blank"
+          >
+            https://app.queroajudar.org
+          </a>
+          <br />
+          Icons from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">
+            www.flaticon.com
+          </a>
+        </p>
         <p>Política de privacidade | Termos de serviço</p>
       </div>
     </footer>
